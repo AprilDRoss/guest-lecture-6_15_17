58f9f3cc266864bbec37fe1c59aadce91c44b0a3
April Ross <aprilross@aprils-mbp.lan>
2017-06-15 14:46:48 -0400

    files

---
+++ webchat.js
@@ -0,0 +1,28 @@
+console.log("Webchat client is running ...");
+var messageList = [];
+function testFetch() {
+	console.log("Testing ...");
+}
+
+function sendMessage() {
+    var userMessage = document.getElementById('user-message').value;
+    messageList.unshift(userMessage);
+    console.log("About to send " + userMessage);
+    console.log("messageList = ");
+    console.log(messageList);
+
+    }
+    
+function renderList(listToRender) {
+  var messageHistoryList = document.getElementById('message-history');
+  var messageHistoryHTML = "";
+  renderList.forEach(function(message) {
+  messageHistoryHTML += "<li class='list-group-item'>" + message + "</li>";
+});
+
+console.log("message history HTML = ");
+console.log(messageHistoryHTML);
+
+messageHistoryList.innerHTML = messageHistoryHTML;
+
+}
